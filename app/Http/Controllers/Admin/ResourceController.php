b0346194b1e7a392158ff9e67f3a4d619779e105
kevinokello <okellokevin77@gmail.com>
2022-09-19 12:35:42 +0300

    resource crud

--- app/Http/Controllers/Admin/ResourceController.php
+++ app/Http/Controllers/Admin/ResourceController.php
@@ -2,31 +2,41 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Category;
 use App\Models\Resource;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Admin\ResourceFormRequest;
 
 class ResourceController extends Controller
 {
     public function View()
     {
-        return view('dashboard.resource.all');
+        $resources = Resource::all();
+        return view('dashboard.resource.all', compact('resources'));
     }
     public function Create()
     {
+        $category = Category::where('status', 'enabled')->get();
+        $subcategory = SubCategory::where('status', 'enabled')->get();
         return view('dashboard.resource.add');
     }
-    public function Store()
+    public function Store(ResourceFormRequest $request)
     {
-
+        $data = $request->validated();
+        $resource = new Resource;
+        $resource->created_by = Auth::user()->id;
+        $resource->save();
     }
-    public function Edit()
+    public function Edit($resource_id)
     {
-
+        $resource = Resource::find($resource_id);
     }
-    public function Update()
+    public function Update(ResourceFormRequest $resource, $resource_id)
     {
-
+        
     }
     public function Destroy($resource_id)
     {
---
+++ resources/views/dashboard/resource/add.blade.php
@@ -0,0 +1,111 @@
+@extends('layouts.dash')
+@section('content')
+    <div class="app-content content container-fluid">
+        <div class="content-wrapper">
+            <div class="content-body">
+                <!-- Basic form layout section start -->
+                <section id="basic-form-layouts">
+                    <div class="row match-height">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body collapse in">
+                                    <div class="card-block">
+
+                                        <form method="post" action="{{ url('resource/category/store') }}" id="myForm"
+                                            class="form" enctype="multipart/form-data">
+                                            @csrf
+                                            <div class="form-body">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label for="projectinput1"> Name</label>
+                                                            <input type="text" class="form-control"
+                                                                placeholder="Category Name" name="name">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label for="projectinput4">Slug</label>
+                                                            <input type="text" class="form-control" placeholder="Slug"
+                                                                name="slug">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label for="projectinput4">Image</label>
+                                                            <input type="file" class="form-control" name="image">
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+
+
+
+                                            </div>
+
+                                            <button
+                                                style="  border-color: #0073aa;
+            background-color: #0073aa;
+            color: #fff; height:50px; width:170px; "
+                                                type="submit" class="btn btn-primary">
+                                                Add category
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!-- // Basic form layout section end -->
+            </div>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#myForm').validate({
+                rules: {
+                    name: {
+                        required: true,
+                    },
+                    supplier_id: {
+                        required: true,
+                    },
+                    unit_id: {
+                        required: true,
+                    },
+                    category_id: {
+                        required: true,
+                    },
+                },
+                messages: {
+                    name: {
+                        required: 'Please Enter Your Product Name',
+                    },
+                    supplier_id: {
+                        required: 'Please Select One Supplier',
+                    },
+                    unit_id: {
+                        required: 'Please Select One Unit',
+                    },
+                    category_id: {
+                        required: 'Please Select One Category',
+                    },
+                },
+                errorElement: 'span',
+                errorPlacement: function(error, element) {
+                    error.addClass('invalid-feedback');
+                    element.closest('.form-group').append(error);
+                },
+                highlight: function(element, errorClass, validClass) {
+                    $(element).addClass('is-invalid');
+                },
+                unhighlight: function(element, errorClass, validClass) {
+                    $(element).removeClass('is-invalid');
+                },
+            });
+        });
+    </script>
+@endsection
